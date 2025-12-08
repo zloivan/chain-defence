5c487ba38467a75ad0e80f428f44229804d2f0ef
ivankhomichkov <ivan.khomichkov@gmail.com>
2025-12-08 05:59:41 +0400

    ADD: initial project install

---
+++ Assets/PlayerPrefsEditor/Editor/PreferencesEditor/PreferenceEntryHolder.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2025 Cyber Chaos Games. All Rights Reserved.
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CCG.PlayerPrefsEditor
+{
+    [System.Serializable]
+    public class PreferenceEntryHolder : ScriptableObject
+    {
+        public List<PreferenceEntry> userDefList;
+        public List<PreferenceEntry> unityDefList;
+
+        private void OnEnable()
+        {
+            hideFlags = HideFlags.DontSave;
+            if (userDefList == null)
+                userDefList = new List<PreferenceEntry>();
+            if (unityDefList == null)
+                unityDefList = new List<PreferenceEntry>();
+        }
+
+        public void ClearLists()
+        {
+            if (userDefList != null)
+                userDefList.Clear();
+            if (unityDefList != null)
+                unityDefList.Clear();
+        }
+    }
+}
---
+++ Assets/SelectionHistory/changelog.md
@@ -0,0 +1,18 @@
+﻿1.0.2
+
+Changed:
+- Selection is now also recorded when the Selection History window is not visible.
+
+1.0.1
+
+Added:
+- Package description and assembly definition, allowing the tool to be used as a package
+
+Changed:
+- Selecting an item in the list will now ping the object to highlight it in the hierarchy/project window
+
+Fixed:
+- Unnecessary slowdown when window is open due to UI constantly redrawing
+
+1.0.0
+Initial release
