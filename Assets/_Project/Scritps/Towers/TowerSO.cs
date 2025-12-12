de8f9999a1fe0edc3f6ad6b70a4429d7ee1a2726
ivankhomichkov <ivan.khomichkov@gmail.com>
2025-12-12 13:22:47 +0400

    FIX: add attack type enum and update tower configs

--- Assets/_Project/Scritps/Towers/TowerSO.cs
+++ Assets/_Project/Scritps/Towers/TowerSO.cs
@@ -2,12 +2,25 @@ using UnityEngine;
 
 namespace ChainDefense.Towers
 {
+    public enum TowerAttackType
+    {
+        SingleTarget,
+        AOE,
+        Slow,
+        Chain
+    }
+    
     [CreateAssetMenu(fileName = "Tower", menuName = "Configs/Tower SO", order = 0)]
     public class TowerSO : ScriptableObject//TODO: Figure out he way to add effects differently per tower not via enum
     {
         public int BaseDamage;
         public float BaseAttackRange;
         public float BaseAttackSpeed;
+        public TowerAttackType AttackType;
+        public float AoeRadius;
+        public float SlowPercentage;
+        public int ChainTargets;
+        public float SlowDuration;
         public GameObject TowerPrefab;
     }
 }
